a8585c8c8d9470a848254f1414a82f6582309b96
Otso Aksela <otso.aksela@gmail.com>
2023-01-06 13:18:37 +0200

    Update effect.js

--- effect/effect.js
+++ effect/effect.js
@@ -1,15 +1,19 @@
-const images = document.querySelectorAll(".anim");
+const animatedElements = document.querySelectorAll(".anim");
 
 observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
         if (entry.intersectionRatio > 0) {
-            entry.target.style.animation = `${entry.target.dataset.type || "fade-down"} ${entry.target.dataset.duration || "1s"} ${entry.target.dataset.delay || "0s"} forwards ${entry.target.dataset.curve || "ease-out"}`;//takes the values lmao, the second behind the || is the default
+            entry.target.style.animation = `
+                ${entry.target.dataset.type || "fade-down"} 
+                ${entry.target.dataset.duration || "1s"} 
+                ${entry.target.dataset.delay || "0s"} forwards 
+                ${entry.target.dataset.curve || "ease-out"}`;
         } else {
             entry.target.style.animation = 'none';
         }
-    })
-})
+    });
+});
 
-images.forEach(image => {
-    observer.observe(image)
-})
+animatedElements.forEach((element) => {
+    observer.observe(element)
+});
